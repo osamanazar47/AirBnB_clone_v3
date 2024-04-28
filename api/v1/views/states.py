024ebc165ded8846a8ec0eba08ca0453c91a6b05
osamanazar47 <osamanazar47@gmail.com>
2024-04-28 12:17:27 +0300

    states_api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,24 +7,31 @@ from models import storage
 from models.state import State
 from flask import jsonify, abort, request, make_response
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """
     Retrieves a State object by state_id
-    - If the state_id is not linked to any State object, a 404 error will be raised
+    - If the state_id is not linked to any State object,
+    a 404 error will be raised
     """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)  # Raise a 404 Not Found error if state is not found
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+d = 'DELETE'
+
+
+@app_views.route('/states/<state_id>', methods=[d], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a specific State object with id (state_id)"""
     state = storage.get(State, state_id)
@@ -33,6 +40,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -45,6 +53,7 @@ def create_state():
     new_state = State(**json_data)
     new_state.save()
     return make_response(jsonify(new_state.to_dict()), 201)
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
